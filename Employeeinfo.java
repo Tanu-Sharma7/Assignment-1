f7dbe9446beb3f7663a742efaa60d2b792fa72e3
Tanu Sharma <160379781+Tanu-Sharma7@users.noreply.github.com>
2025-02-09 10:44:27 +0530

    Add files via upload

---
+++ Employeeinfo.java
@@ -0,0 +1,10 @@
+//write a program which prints the following information at least 5 persons: NAME,MAIL-ID,EMPLOYEE-CODE,PHONE
+class Employeeinfo {
+    public static void main(String[] args) {
+        System.out.println("Rahul Sharma     rahul.sharma@gmail.com        E1001           9876543210");
+        System.out.println("Anjali Verma     anjali.verma@gmail.com        E1002           9123456789");
+        System.out.println("Vikram Patel     vikram.patel@gmail.com        E1003           9988776655");
+        System.out.println("Priya Iyer       priya.iyer@gmail.com          E1004           9556677889");
+        System.out.println("Arjun Reddy      arjun.reddy@gmail.com         E1005           9112233445");
+    }
+}
---
+++ Printwithquotes.java
@@ -0,0 +1,6 @@
+//write a program that prints the following line on the screen along with the quotes:  "can we print '\' with system.out.println() statement?"
+public class Printwithquotes {
+    public static void main(String[] args) {
+        System.out.println("\"can we print '\\' with system.out.println() statement?\"");
+    }
+}
